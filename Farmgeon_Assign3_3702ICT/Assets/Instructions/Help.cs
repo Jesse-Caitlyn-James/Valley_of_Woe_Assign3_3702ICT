4582465cc1d328f8489797013a836139c3e0d16a
Jesse Caitlyn James <jesse.caitlyn.james20@gmail.com>
2024-10-13 20:06:23 +1000

    Update: Small fix

---
+++ Farmgeon_Assign3_3702ICT/Assets/Instructions/Help.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Help : MonoBehaviour
+{
+    public Button backButton;
+
+    void Start()
+    {
+        
+        if (backButton != null)
+        {
+            backButton.onClick.AddListener(OnBackButtonPressed);
+        }
+        
+    }
+
+    public void OnBackButtonPressed()
+    {
+        
+        SceneManager.LoadScene("Menu");
+    }
+}
